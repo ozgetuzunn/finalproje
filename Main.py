01aa852866efb073eb08f03875bedde7b5eac209
ozgetuzunn <101674371+ozgetuzunn@users.noreply.github.com>
2023-06-08 16:30:28 +0300

    Update Main.py
    işsiz

--- Main.py
+++ Main.py
@@ -46,3 +46,34 @@ class Insan:
     def __str__(self):
         return f"TC No: {self.__tc_no}\nAd: {self.__ad}\nSoyad: {self.__soyad}\nYaş: {self.__yas}\nCinsiyet: {self.__cinsiyet}\nUyruk: {self.__uyruk}"
     
+class Issiz(Insan):
+    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, tecrube):
+        super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk)
+        self.__tecrube = tecrube
+        self.__status = self.statu_bul()
+
+    def set_status(self, status):
+        self.__status = status
+
+    def get_status(self):
+        return self.__status
+
+    def statu_bul(self):
+        try:
+            m = self.__tecrube["mavi_yaka"] * 0.2
+            b = self.__tecrube["beyaz_yaka"] * 0.35
+            y = self.__tecrube["yonetici"] * 0.45
+            if m > b and m > y:
+                self.__status = "mavi_yaka"
+            elif b > m and b > y:
+                self.__status = "beyaz_yaka"
+            elif y > m and y > b:
+                self.__status = "yonetici"
+            else:
+                self.__status = "mavi_yaka"
+            return self.__status
+        except:
+            print("Bir hata oluştu!")
+
+    def __str__(self):
+        return f"{super().__str__()}\nStatü: {self.__status}"
