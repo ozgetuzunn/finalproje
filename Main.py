5ccda41de3440389784086095322b7d4b49e8253
ozgetuzunn <101674371+ozgetuzunn@users.noreply.github.com>
2023-06-08 16:30:29 +0300

    Update Main.py
    beyazyaka

--- Main.py
+++ Main.py
@@ -118,7 +118,7 @@ class Calisan(Insan):
             print("Bir hata oluştu!")
 
     def __str__(self):
-        return f"{super().__str__()}\nSektör: {self.__sektor}\nTecrübe: {self.__tecrube}\nYeni Maaş: {self.__maas * (1 + self.zam_hakki()/100)}"
+        return f"Ad: {self.get_ad()}\nSoyad: {self.get_soyad()}\nTecrübe: {self.__tecrube}\nYeni Maaş: {self.__maas * (1 + self.zam_hakki()/100)}"
     
 """Mavi yaka sınıfı için (MaviYaka.py) yıpranma payı (float: 0.2, 0.5 gibi değer almalıdır) değişkeni private olarak bulunmalıdır.
 • Değişkenlere göre Initializer metot olmalıdır.
@@ -152,5 +152,34 @@ class MaviYaka(Calisan):
             print("Bir hata oluştu!")
 
 
+
+
+class BeyazYaka(Calisan):
+    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas, tesvik_primi):
+        super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas)
+        self.__tesvik_primi = tesvik_primi
+
+    def set_tesvik_primi(self, tesvik_primi):
+        self.__tesvik_primi = tesvik_primi
+
+    def get_tesvik_primi(self):
+        return self.__tesvik_primi
+
+    def zam_hakki(self):
+        try:
+            if self.get_tecrube() < 24:
+                return self.__tesvik_primi
+            elif self.get_tecrube() >= 24 and self.get_tecrube() <= 48 and self.get_maas() < 15000:
+                return (self.get_maas() % self.get_tecrube()) * 5 + self.__tesvik_primi
+            elif self.get_tecrube() > 48 and self.get_maas() < 25000:
+                return (self.get_maas() % self.get_tecrube()) * 4 + self.__tesvik_primi
+            else:
+                return 0
+        except:
+            print("Bir hata oluştu!")
+
+
+    def __str__(self):
+        return f"Ad: {self.get_ad()}\nSoyad: {self.get_soyad()}\nTecrübe: {self.__tecrube}\nYeni Maaş: {self.__maas + self.zam_hakki()}"
 
 
